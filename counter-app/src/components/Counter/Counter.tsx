25240926c6719d2de92dc3946e440eb6c961aec0
Khairul <Khairulimran6810@gmail.com>
2024-11-02 17:25:32 +0800

    Added feater to store counter value in localStorage.

--- counter-app/src/components/Counter/Counter.tsx
+++ counter-app/src/components/Counter/Counter.tsx
@@ -1,13 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Button from '../Button/Button'
 import CounterDisplay from "./CounterDisplay";
 
 function Counter() {
-    const [count, setCount] = useState<number>(0);
+    // Initialise state from localStorage or default to 0
+    const [count, setCount] = useState<number>(() => {
+        const savedCount = localStorage.getItem('count');
+        return savedCount ? JSON.parse(savedCount) : 0;
+    });
 
     const increment = () => setCount(prev => prev + 1);
     const decrement = () => setCount(prev => prev - 1);
     const reset = () => setCount(0);
+
+    // to save count to localStorage whenever it changes
+    useEffect(() => {
+        localStorage.setItem('count', JSON.stringify(count));
+    }, [count]); // Dependency array - useEffect runs when these values change
 
     return (
         <div className="flex flex-col items-center gap-4">

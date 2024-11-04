7e20e0a41618d26daab05ac74820c1dbd3a9f49c
Khairul <Khairulimran6810@gmail.com>
2024-11-04 23:45:00 +0800

    Added to TodoList.tsx, and added configuration stuff for tailwind.

--- todolist-app/src/components/TodoList.tsx
+++ todolist-app/src/components/TodoList.tsx
@@ -1,4 +1,5 @@
 import { Todo } from "../types/todo";
+import TodoItem from "./TodoItem";
 
 interface TodoListProps {
     todos: Todo[],
@@ -8,11 +9,25 @@ interface TodoListProps {
 
 function TodoList({ todos, onToggleTodo, onDeleteTodo }: TodoListProps) {
 
+    if (todos.length === 0) {
+        return (
+            <div className="p-4 text-center text-gray-500">
+                No todos add yet. Add one above!
+            </div>
+        );
+    }
 
     return (
-        <div>
-            
-        </div>
+        <ul className="divide-y divide-gray-200">
+            {todos.map((todo) => (
+                <TodoItem
+                    key={todo.id}
+                    todo={todo}
+                    onToggle={onToggleTodo}
+                    onDelete={onDeleteTodo}
+                />  
+            ))}
+        </ul>
     );
 }
 

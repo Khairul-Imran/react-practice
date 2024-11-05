346654c75c43d3677448103392718d41f95ddd8b
Khairul <Khairulimran6810@gmail.com>
2024-11-05 15:34:52 +0800

    Added localStorage saving for todos.

--- todolist-app/src/App.tsx
+++ todolist-app/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import TodoForm from "./components/TodoForm";
 import TodoHeader from "./components/TodoHeader";
@@ -6,8 +6,13 @@ import TodoList from "./components/TodoList";
 import { Todo } from "./types/todo";
 
 function App() {
+
     // State lives in the parent
-    const [todos, setTodos] = useState<Todo[]>([]);
+    // Initialise state from localStorage or default to empty array
+    const [todos, setTodos] = useState<Todo[]>(() => {
+      const savedTodos = localStorage.getItem('todos');
+      return savedTodos ? JSON.parse(savedTodos) : [];
+    });
 
     // Callback function defined in parent
     const addTodo = (text: string) => {
@@ -21,30 +26,34 @@ function App() {
         ]);
     };
 
-    // To revise these concepts to better understand.
+    // TODO: To revise these concepts to better understand.
     // Functions/common methods
     const toggleTodo = (id: number) => {
-      setTodos(todos.map(todo => todo.id === id ? { ...todo, completed: !todo.completed } : todo));
+        setTodos(
+            todos.map((todo) =>
+                todo.id === id ? { ...todo, completed: !todo.completed } : todo
+            )
+        );
     };
 
     const deleteTodo = (id: number) => {
-      setTodos(todos.filter(todo => todo.id !== id));
+        setTodos(todos.filter((todo) => todo.id !== id));
     };
+
+    // To save todos in localStorage whenever todolist changes
+    useEffect(() => {
+      localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos]);
 
     return (
         <div className="min-h-screen bg-gray-100 py-8">
-            <div className="container mx-auto px-4">
-                <h1 className="text-3xl font-bold text-center mb-8">
-                    Counter App
-                </h1>
-                <div className="max-w-md mx-auto bg-white p-8 rounded-lg shadow-md"></div>
-                <TodoHeader></TodoHeader>
-                <TodoForm onAddTodo={addTodo}></TodoForm>
-                <TodoList 
-                  todos={todos}
-                  onToggleTodo={toggleTodo}
-                  onDeleteTodo={deleteTodo}></TodoList>
-            </div>
+            <TodoHeader></TodoHeader>
+            <TodoForm onAddTodo={addTodo}></TodoForm>
+            <TodoList
+                todos={todos}
+                onToggleTodo={toggleTodo}
+                onDeleteTodo={deleteTodo}
+            ></TodoList>
         </div>
     );
 }
